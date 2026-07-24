f852b363f0f2b0ee1fbc0977f6829ce6f20575dd
Samuel N Henry Jr. <samuelnhenryjr@gmail.com>
2026-07-24 15:22:45 +0530

    Add files via upload

---
+++ Unit 1/array.java
@@ -0,0 +1,25 @@
+//10.Write a java program of 1D array 
+
+import java.util.Scanner;
+class array{
+	public static void main(String[]args)
+	{   
+	//Array declaration.
+		int Score[] = new int[5];
+		
+		Score[0] = 90;
+		Score[1] = 92;
+		Score[3] = 88;
+		Score[4] = 76;
+		Score[5] = 99;
+		
+		System.out.println("\nManually assigned array elements:"\n);
+		
+		for(int i=0; i<score.length; i++){
+			System.out.println(Scores[i] +" ");
+		}	
+	
+	}
+}
+		
+		
