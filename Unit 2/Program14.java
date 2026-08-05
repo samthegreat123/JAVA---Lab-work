91968a5673add32a388642bffa5b2cc8aae7c580
Samuel N Henry Jr. <samuelnhenryjr@gmail.com>
2026-08-05 15:52:04 +0530

    Add files via upload

---
+++ Unit 2/Program14.java
@@ -0,0 +1,32 @@
+/* Inheritance example*/
+
+class Animal{
+     String name ="Animal";
+     void eat(){
+       System.out.println("This Animal eats food.");
+     }
+}
+
+class Dog extends Animal{
+    void bark(){
+        System.out.println("The dog barks: Wolf! Wolf!");
+    }
+}
+
+  class Poppy extends Dog{
+    void weep(){
+        System.out.println("The Pups Weeps!");
+    }
+  }
+
+    public class Program14{
+        public static void main(String[] Args)
+        {
+            Poppy mypups = new Poppy();
+            mypups.eat();
+            mypups.bark();
+            mypups.weep();
+        }
+    }
+
+
