d01405c7e425ba50f6fa194a3536c8cee44c7dcb
Samuel N Henry Jr. <samuelnhenryjr@gmail.com>
2026-08-22 04:57:21 +0530

    Add files via upload

---
+++ Unit 2/Abstract.java
@@ -0,0 +1,32 @@
+abstract class Animal
+{
+	//abstract method(this method does not have a method).
+	public abstract void animalsound();
+	
+	void sleep()
+	{
+		System.out.println("Zzzzzzzzz");
+	}
+	
+}
+   class Dog extends Animal
+   {
+	   public void animalsound()
+	   {
+		   System.out.println("The Dog says: word..Worf");
+		   
+	   }
+	   
+   }
+   
+   class Abs
+   {
+	   public static void main(String[]args)
+	   {
+		   Dog myDog = new Dog();
+		   
+		   myDog.animalsound();
+		   myDog.sleep();
+	   }
+	   
+   }
